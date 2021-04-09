3a1297812c3b73269cb225de5e5f1c3523b63059
Aldo Rivera <77871817+aldoorivera@users.noreply.github.com>
2021-04-08 18:34:24 -0600

    Creacion de estilo

---
+++ src/cmps/Footer/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+    return(
+        <div className="flex flex-row items-center sm:px-80 sm:text-lg md:px-40 md:text-3xl p-2 justify-between" >
+            <a >Facebook</a>
+            <a>Twitter</a>
+            <a>Instagram</a>
+        </div>
+    )
+}
+
+export default Footer
