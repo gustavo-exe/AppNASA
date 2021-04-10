cc22a8dba27ec3582339cc4041c0e3849014c94f
ArielMejia <arielgomezcr01@gmail.com>
2021-04-09 23:00:41 -0600

    Creacion del componente base para la Galeria

---
+++ src/cmps/Galeria/Galeria.js
@@ -0,0 +1,14 @@
+import NavBar from '../NavBar/NavBar';
+import Footer from '../Footer/Footer';
+
+function Galeria(){
+    return(
+        <div>
+            <NavBar/>
+            
+            <Footer/>
+        </div>
+    )
+}
+
+export default Galeria;
