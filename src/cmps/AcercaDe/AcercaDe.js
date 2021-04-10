b0ac413c3288cf171e0573a9897c781e750f0c41
OValle418 <61193370+OValle418@users.noreply.github.com>
2021-04-09 22:58:21 -0600

    Creacion del componente acercaDe

---
+++ src/cmps/AcercaDe/AcercaDe.js
@@ -0,0 +1,13 @@
+import NavBar from '../NavBar/NavBar';
+import Footer from '../Footer/Footer';
+function AcercaDe() {
+    return(
+    <div className="flex flex-col justify-between">
+        <NavBar/>
+        <div>
+        
+        </div>
+        <Footer/>
+    </div>)
+}
+export default AcercaDe;
