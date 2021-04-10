9ed39ee47f89ab77b11354bbe6915c93bc68c659
moralesklisban9 <moralesklisban@gmail.com>
2021-04-09 23:48:18 -0600

    avance galeria
    avanzando en la galeria de NASA

--- src/cmps/Galeria/Galeria.js
+++ src/cmps/Galeria/Galeria.js
@@ -1,11 +1,65 @@
 import NavBar from '../NavBar/NavBar';
 import Footer from '../Footer/Footer';
+import Tarjeta from './Tarjeta';
 
 function Galeria(){
     return(
         <div>
             <NavBar/>
-            
+            <div className=" flex md:flex-row justify-items-center flex-col justify-between py-8 px-6">
+                <Tarjeta
+                    imagen="https://raw.githubusercontent.com/gustavo-exe/AppNASA/master/src/img/firewal%20nova.jpg"
+                    titulo="bby"
+                    descripcion="hola mundo"
+                />
+                <Tarjeta
+                    imagen="https://raw.githubusercontent.com/gustavo-exe/AppNASA/master/src/img/firewal%20nova.jpg"
+                    titulo="bby"
+                    descripcion="hola mundo"
+                />
+                <Tarjeta
+                    imagen="https://raw.githubusercontent.com/gustavo-exe/AppNASA/master/src/img/firewal%20nova.jpg"
+                    titulo="bby"
+                    descripcion="hola mundo"
+                />
+            </div>
+
+            <div className=" flex md:flex-row justify-items-center flex-col justify-between py-8 px-6">
+                <Tarjeta
+                    imagen="https://raw.githubusercontent.com/gustavo-exe/AppNASA/master/src/img/firewal%20nova.jpg"
+                    titulo="bby"
+                    descripcion="hola mundo"
+                />
+                <Tarjeta
+                    imagen="https://raw.githubusercontent.com/gustavo-exe/AppNASA/master/src/img/firewal%20nova.jpg"
+                    titulo="bby"
+                    descripcion="hola mundo"
+                />
+                <Tarjeta
+                    imagen="https://raw.githubusercontent.com/gustavo-exe/AppNASA/master/src/img/firewal%20nova.jpg"
+                    titulo="bby"
+                    descripcion="hola mundo"
+                />
+            </div>
+
+            <div className=" flex md:flex-row justify-items-center flex-col justify-between py-8 px-6">
+                <Tarjeta
+                    imagen="https://raw.githubusercontent.com/gustavo-exe/AppNASA/master/src/img/firewal%20nova.jpg"
+                    titulo="bby"
+                    descripcion="hola mundo"
+                />
+                <Tarjeta
+                    imagen="https://raw.githubusercontent.com/gustavo-exe/AppNASA/master/src/img/firewal%20nova.jpg"
+                    titulo="bby"
+                    descripcion="hola mundo"
+                />
+                <Tarjeta
+                    imagen="https://raw.githubusercontent.com/gustavo-exe/AppNASA/master/src/img/firewal%20nova.jpg"
+                    titulo="bby"
+                    descripcion="hola mundo"
+                />
+            </div>
+
             <Footer/>
         </div>
     )
---
+++ src/cmps/Galeria/Tarjeta.js
@@ -0,0 +1,17 @@
+function Tarjeta({imagen, titulo, descripcion}) {
+    return(
+        <div className="max-w-xs rounded overflow-hidden shadow-lg my-2 mx-2">
+            <img className="w-full" src={imagen}></img>
+            <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">{titulo}</div>
+                <p>
+                    {descripcion}
+                </p>
+            </div>
+            <div className="px-6 py-4">
+            </div>
+        </div>
+    )
+}
+
+export default Tarjeta;
