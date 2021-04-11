85c05f9b552b4039cce6cec3b2b5995d3e1e42ff
ArielMejia <arielgomezcr01@gmail.com>
2021-04-11 17:03:12 -0600

    Mejora en las tarjetas de galeria

--- src/cmps/Galeria/Tarjeta.js
+++ src/cmps/Galeria/Tarjeta.js
@@ -1,6 +1,8 @@
+import '../../css/Tarjeta.css';
+
 function Tarjeta({imagen, titulo, descripcion}) {
     return(
-        <div className="max-w-xs rounded overflow-hidden shadow-lg my-2 mx-2">
+        <div className="Tarjeta max-w-xs rounded overflow-hidden shadow-lg my-2 mx-2">
             <img className="w-full" src={imagen}></img>
             <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">{titulo}</div>
